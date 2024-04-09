41d40172b99b2e0a81d5214f720d9e1e49b1b6f7
Tanmoy Deb <tanmoydeb2002@gmail.com>
2024-04-09 19:23:57 +0530

    list user

---
+++ frontend/src/screens/UserListScreen.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { Store } from '../Store';
+import { getError } from '../utils';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+export default function UserListScreen() {
+  const [{ loading, error, users }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/users`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(err),
+        });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+  return (
+    <div>
+      <Helmet>
+        <title>Users</title>
+      </Helmet>
+      <h1>Users</h1>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">NAME</th>
+              <th className="px-4 py-2">EMAIL</th>
+              <th className="px-4 py-2">IS ADMIN</th>
+              <th className="px-4 py-2">ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id} className="text-center border">
+                <td className="px-4 py-2">{user._id}</td>
+                <td className="px-4 py-2">{user.name}</td>
+                <td className="px-4 py-2">{user.email}</td>
+                <td className="px-4 py-2">{user.isAdmin ? 'YES' : 'NO'}</td>
+                <td className="px-4 py-2"></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
