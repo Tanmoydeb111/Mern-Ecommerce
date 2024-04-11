25c83bbfdfd81183f34390e0853eb88ee432f4d4
Tanmoy Deb <tanmoydeb2002@gmail.com>
2024-04-11 20:51:45 +0530

    add review

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,24 @@
 import axios from 'axios';
-import { useContext, useEffect, useReducer, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useContext, useEffect, useReducer, useRef, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
 import { Store } from '../Store';
+import { toast } from 'react-toastify';
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'REFRESH_PRODUCT':
+      return { ...state, product: action.payload };
+    case 'CREATE_REQUEST':
+      return { ...state, loadingCreateReview: true };
+    case 'CREATE_SUCCESS':
+      return { ...state, loadingCreateReview: false };
+    case 'CREATE_FAIL':
+      return { ...state, loadingCreateReview: false };
     case 'FETCH_REQUEST':
       return { ...state, loading: true };
     case 'FETCH_SUCCESS':
@@ -22,15 +31,20 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  let reviewsRef = useRef();
+
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
   const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
 
-  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
-    product: [],
-    loading: true,
-    error: '',
-  });
+  const [{ loading, error, product, loadingCreateReview }, dispatch] =
+    useReducer(reducer, {
+      product: [],
+      loading: true,
+      error: '',
+    });
   const [size, setSize] = useState('');
   // const [color, setColor] = useState('');
   useEffect(() => {
@@ -47,7 +61,7 @@ function ProductScreen() {
   }, [slug]);
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart } = state;
+  const { cart, userInfo } = state;
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
@@ -61,6 +75,39 @@ function ProductScreen() {
       payload: { ...product, quantity, size },
     });
     navigate('/cart');
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!comment || !rating) {
+      toast.error('Please enter comment and rating');
+      return;
+    }
+    try {
+      const { data } = await axios.post(
+        `/api/products/${product._id}/reviews`,
+        { rating, comment, name: userInfo.name },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+
+      dispatch({
+        type: 'CREATE_SUCCESS',
+      });
+      toast.success('Review submitted successfully');
+      product.reviews.unshift(data.review);
+      product.numReviews = data.numReviews;
+      product.rating = data.rating;
+      dispatch({ type: 'REFRESH_PRODUCT', payload: product });
+      window.scrollTo({
+        behavior: 'smooth',
+        top: reviewsRef.current.offsetTop,
+      });
+    } catch (error) {
+      toast.error(getError(error));
+      dispatch({ type: 'CREATE_FAIL' });
+    }
   };
 
   const [selectedSize, setSelectedSize] = useState(
@@ -166,6 +213,92 @@ function ProductScreen() {
                 </>
               )}
             </ul>
+            <div className="my-6">
+              <h2 className="text-2xl font-bold mb-3" ref={reviewsRef}>
+                Reviews
+              </h2>
+              {product.reviews.length === 0 && (
+                <div className="text-center py-3">There is no review</div>
+              )}
+              <ul>
+                {product.reviews.map((review) => (
+                  <li key={review._id} className="mb-3 p-3 shadow-lg">
+                    <strong className="font-bold">{review.name}</strong>
+                    <Rating rating={review.rating} caption=" "></Rating>
+                    <p className="text-sm">
+                      {review.createdAt.substring(0, 10)}
+                    </p>
+                    <p>{review.comment}</p>
+                  </li>
+                ))}
+              </ul>
+              <div className="my-6">
+                {userInfo ? (
+                  <form onSubmit={submitHandler}>
+                    <h2 className="text-xl font-bold mb-3">
+                      Write a customer review
+                    </h2>
+                    <div className="mb-3">
+                      <label
+                        className="block text-sm font-medium mb-1"
+                        htmlFor="rating"
+                      >
+                        Rating
+                      </label>
+                      <select
+                        id="rating"
+                        className="block w-full p-2 border border-gray-300 rounded-md"
+                        value={rating}
+                        onChange={(e) => setRating(e.target.value)}
+                      >
+                        <option value="">Select...</option>
+                        <option value="1">1- Poor</option>
+                        <option value="2">2- Fair</option>
+                        <option value="3">3- Good</option>
+                        <option value="4">4- Very good</option>
+                        <option value="5">5- Excelent</option>
+                      </select>
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        className="block text-sm font-medium mb-1"
+                        htmlFor="floatingTextarea"
+                      >
+                        Comments
+                      </label>
+                      <textarea
+                        id="floatingTextarea"
+                        className="block w-full p-2 border border-gray-300 rounded-md"
+                        placeholder="Leave a comment here"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                      />
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <button
+                        disabled={loadingCreateReview}
+                        type="submit"
+                        className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 disabled:opacity-50"
+                      >
+                        Submit
+                      </button>
+                      {loadingCreateReview && <div>Loading...</div>}
+                    </div>
+                  </form>
+                ) : (
+                  <div className="text-center py-3">
+                    Please{' '}
+                    <Link
+                      to={`/signin?redirect=/product/${product.slug}`}
+                      className="text-blue-500 hover:text-blue-600"
+                    >
+                      Sign In
+                    </Link>{' '}
+                    to write a review
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
